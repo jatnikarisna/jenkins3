5719efccc6de545f5344d232aec687fd567941d4
jatnikarisna <jatnikarisna@gmail.com>
2025-02-01 21:22:23 +0700

    Jenkins

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - MOAI ENM - v16.0');
+  res.send('Hello World - MOAI ENM - CI/CD');
 });
 
 app.listen(PORT, HOST, () => {
