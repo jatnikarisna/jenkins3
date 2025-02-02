5ad5690bb1e82f8204a3d3c2842ca89ef2fdf3bb
jatnikarisna <jatnikarisna@gmail.com>
2025-02-02 21:11:04 +0700

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - MOAI ENM - CI/CD - The Diagram - V1 - Feb, 02 2025');
+  res.send('Hello World - MOAI ENM - CI/CD - The Diagram - V2 - Feb, 02 2025');
 });
 
 app.listen(PORT, HOST, () => {
