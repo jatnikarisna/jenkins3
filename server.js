2caed5543e0137db6f4b752ffb20e430fbdae1c1
jatnikarisna <jatnikarisna@gmail.com>
2025-02-06 14:31:46 +0700

    test 02062025

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Jenkins - CI/CD - Feb, 06 2025');
+  res.send('Hello World - Jenkins - CI/CD - Feb, 06 2025 - V1');
 });
 
 app.listen(PORT, HOST, () => {
